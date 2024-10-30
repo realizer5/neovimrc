b82b0b7f10d87025b803583146aae21dc63547bb
Realizer <107213639+realizer5@users.noreply.github.com>
2024-10-30 08:02:35 +0530

    Add files via upload

---
+++ plugins/colors.lua
@@ -0,0 +1,13 @@
+return{
+	{
+		"rose-pine/neovim",
+		name = "rose-pine" ,
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {italic = false,}
+			})
+			vim.cmd.colorscheme("rose-pine")
+		end
+	}
+}
