07ed8671f375882cc7158974c716c300f55ddee9
Realizer <107213639+realizer5@users.noreply.github.com>
2024-12-12 10:19:01 +0530

    Add files via upload

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -23,6 +23,19 @@ return {
                 -- `false` will disable the whole extension
                 enable = true,
 
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        vim.notify(
+                            "File larger than 100KB treesitter disabled for performance",
+                            vim.log.levels.WARN,
+                            {title = "Treesitter"}
+                        )
+                        return true
+                    end
+                end,
+
                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                 -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
