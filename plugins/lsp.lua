b0181c063b7c02e52ddd50e84596fd757a8793b5
realizer <ipreet35232@gmail.com>
2025-10-24 09:02:50 +0530

    some changes

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -14,6 +14,10 @@ return {
     },
 
     config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+            }
+        })
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -39,6 +43,7 @@ return {
                 "tailwindcss",
                 "ts_ls",
                 "html",
+                "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "javascript", "lua", "bash", "html", "css",
+                "javascript", "lua", "bash", "html", "css", "tsx", "go",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -40,7 +40,7 @@ return {
                 -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
                 -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
+                additional_vim_regex_highlighting = { "markdown" },
             },
         })
     end
